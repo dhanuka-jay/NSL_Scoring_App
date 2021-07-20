3893b95970c973b04fa3d270c7e15944c308b4d3
Dan Ilandarage <dhanuka.singhe@gmail.com>
2021-07-21 00:35:07 +1000

    Toss Done; Total Score Done; BatterShifting Logic is abb

--- src/components/Game/Over.js
+++ src/components/Game/Over.js
@@ -1,19 +1,17 @@
 
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { Button, Dropdown, Modal, Segment, Divider, Label, Icon } from "semantic-ui-react";
 import BallButton from "./BallButton";
 
-const Over = ({teamA, teamB, overList, setOverList}) => {
+const Over = ({teamA, teamB, overList, clubPlayers, setOverList}) => {
     const [open, setOpen] = useState(false);
     const [isBatStatOpen, setIsBatStatOpen] = useState(false);
-    const [isBatTeamOpen, setIsBatTeamOpen] = useState(true);
-    const [battingTeam, setBattingTeam] = useState();
     const [scoreType, setScoreType] = useState('');
     const [runs, setRuns] = useState();
     const [over, setOver] = useState([]);
     const [bowler, setBowler] = useState('');
-    const [batsman1, setBatsman1] = useState();
-    const [batsman2, setBatsman2] = useState();
+    const [batsman1, setBatsman1] = useState('');
+    const [batsman2, setBatsman2] = useState('');
     const [notReady, setNotReady] = useState(true);
     const [currOver, setCurrOver] = useState(0);
     const [currBall, setCurrBall] = useState(1);
@@ -35,20 +33,58 @@ const Over = ({teamA, teamB, overList, setOverList}) => {
     const scoreTypeOptions = [
         { key: 1, text: 'Wide-Ball', value: 'wide' },
         { key: 2, text: 'No-Ball', value: 'no' },
-        { key: 3, text: 'Bies', value: 'bies' }
+        { key: 3, text: 'Byes', value: 'byes' }
       ]
 
-    // const batTeamOptions = [
-    //     key: teamA.
-    // ]
-        
-    const getTeamPlayers = () => {
-        const teamPlayers = teamB.players.filter( plyr => plyr.batStat !== 'OUT')
+    const batsmanOptions = [
+        {key: 1, text: {batsman1}, value: {batsman1}},
+        {key: 2, text: {batsman2}, value: {batsman2}}
+    ]
+
+       
+    /* 
+        getBattingPlayers() => Options for two batsman-dropdowns. 
+        'batNumber' is used to filter out already selected player from the list.
+        ie: if 'Batsman 1' is already selected from the dropdown, he will not be shown in the 'Batsman 2' dropdown.
+    */
+    const getBattingPlayers = (batNumber) => {
+        const battingPlayers = 
+        (batNumber === 1) ?
+        (teamA.batting === true ? teamA : teamB).players.filter( plyr => plyr.batStat !== 'OUT')
+            .filter(player => player.playerID !== batsman2)
             .map( player => 
              ({key: player.playerID, text: `${player.playerFName} ${player.playerLName}`, value: player.playerID})
         )
-        return teamPlayers;
+        :
+        (teamA.batting === true ? teamA : teamB).players.filter( plyr => plyr.batStat !== 'OUT')
+            .filter(player => player.playerID !== batsman1)
+            .map( player => 
+             ({key: player.playerID, text: `${player.playerFName} ${player.playerLName}`, value: player.playerID})
+        )
+        return battingPlayers;
     }
+
+    const getCurrentBatters = () => {
+        const currBatters = clubPlayers
+        .filter(player => (player.playerID === batsman1 || player.playerID === batsman2))
+        // .filter(player => player.playerID === batsman2)
+        .map( player => 
+            ({key: player.playerID, text: `${player.playerFName} ${player.playerLName}`, value: player.playerID})
+        )
+        //console.log(currBatters)
+        return currBatters;
+    }
+
+
+
+    const getBowlingPlayers = () => {
+        const bowlingPlayers = (teamA.batting === true ? teamB : teamA).players
+            .map( player => 
+             ({key: player.playerID, text: `${player.playerFName} ${player.playerLName}`, value: player.playerID})
+        )
+        return bowlingPlayers;
+    }
+
 
     useEffect(() => {
         if(over.length == 6) {
@@ -56,14 +92,16 @@ const Over = ({teamA, teamB, overList, setOverList}) => {
             setOver([]);
         }
 
-        }, [over])
+        getScoreForBatsman();        
+    }, [over])
 
     const handleScore = () => {
-        setOver([...over, {ballNum: currBall, scoreType: scoreType, runs: runs}]);
+        setOver([...over, {ballNum: currBall, scoreType: scoreType, runs: runs, batsman: currBatsman}]);
 
         // Zero-out ball
         setRuns();
         setScoreType('');
+        setCurrBatsman();
 
         setNotReady(true);
         setOpen(false);
@@ -80,6 +118,39 @@ const Over = ({teamA, teamB, overList, setOverList}) => {
         }
     }
 
+
+    /* 
+        getScoreForBatsman() => Return the current score of the batsman
+        Param : 'batsman' => batsmanID
+    */
+    const getScoreForBatsman = () => {
+        //console.log(batsman);
+
+        // upToLastOverScore => Total score up to the Last Ended Over.
+        let upToLastOverScore = 0;
+
+        // thisOverScore => Score in this over.
+        let thisOverScore = 0;
+
+        if(overList.length > 0){
+            overList.forEach((overFromList) => {
+                upToLastOverScore = overFromList.over.reduce((prevOverTotal, ball) => {
+                    return prevOverTotal + ball.runs
+                }, upToLastOverScore)
+            })            
+        }
+
+        if(over.length > 0){
+            thisOverScore = over.reduce((overTotal, ball) => {
+                return overTotal + ball.runs
+            }, 0)
+        } 
+
+        console.log(`last Tot: ${upToLastOverScore}`)
+
+        console.log(`this Over: ${thisOverScore}`);
+    }
+
     return (
         <div className="over-container">
             <div className="teamPlayers-container">
@@ -93,7 +164,6 @@ const Over = ({teamA, teamB, overList, setOverList}) => {
                                 color='green' 
                                 floating
                                 onClick={() => {
-                                        setCurrBatsman(batsman1);
                                         setIsBatStatOpen(true);
                                     }
                                 }
@@ -106,7 +176,7 @@ const Over = ({teamA, teamB, overList, setOverList}) => {
                                 button
                                 clearable
                                 placeholder='Select Batsman 1'                
-                                options={getTeamPlayers()}
+                                options={getBattingPlayers(1)}
                                 value={batsman1}
                                 onChange={(e, {value}) => setBatsman1(value)}
                             />
@@ -121,7 +191,6 @@ const Over = ({teamA, teamB, overList, setOverList}) => {
                                 color='green' 
                                 floating
                                 onClick={() => {
-                                        setCurrBatsman(batsman2);
                                         setIsBatStatOpen(true);
                                     }
                                 }
@@ -134,7 +203,7 @@ const Over = ({teamA, teamB, overList, setOverList}) => {
                                 button
                                 clearable
                                 placeholder='Select Batsman 2'                
-                                options={getTeamPlayers()}
+                                options={getBattingPlayers(2)}
                                 value={batsman2}
                                 onChange={(e, {value}) => setBatsman2(value)}
                             />               
@@ -154,7 +223,7 @@ const Over = ({teamA, teamB, overList, setOverList}) => {
                             button
                             clearable
                             placeholder='Select Bowler'                
-                            options={getTeamPlayers(teamA)}
+                            options={getBowlingPlayers()}
                             value={bowler}
                             onChange={(e, {value}) => setBowler(value)}
                         />
@@ -211,6 +280,7 @@ const Over = ({teamA, teamB, overList, setOverList}) => {
                 }
                 closeOnDimmerClick={true}
             >
+                <Modal.Header>Let's Record this Delivery</Modal.Header>
                 <Modal.Content>
                     <Segment textAlign='center'>                        
                         <Divider horizontal>Runs</Divider>
@@ -235,6 +305,16 @@ const Over = ({teamA, teamB, overList, setOverList}) => {
                                 onChange={(e, {value}) => setScoreType(value)}
                             />
                         </div>
+                        <Divider horizontal>Batsman</Divider>
+                        <div className="score-typeww">
+                            <Dropdown 
+                                options={getCurrentBatters()} 
+                                selection 
+                                clearable
+                                placeholder="Select Batsman"
+                                onChange={(e, {value}) => setCurrBatsman(value)}
+                            />
+                        </div>
                         <Divider horizontal>Confirm</Divider>
                         <Button 
                             color="green"
@@ -249,7 +329,6 @@ const Over = ({teamA, teamB, overList, setOverList}) => {
                 size='mini'
                 open={isBatStatOpen}
                 onClose={() => {
-                        setCurrBatsman();
                         setIsBatStatOpen(false)
                     }
                 }
@@ -261,7 +340,6 @@ const Over = ({teamA, teamB, overList, setOverList}) => {
                         icon 
                         floated='left'
                         onClick={() => {
-                            setCurrBatsman();
                             setIsBatStatOpen(false)
                             }                        
                         }
